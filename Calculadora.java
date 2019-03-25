077b0b4096a8dfafcb389035919633357e22adbe
AdanFT <franco.start.moba@gmail.com>
2019-03-25 17:50:57 -0400

    funcion suma completada

--- Calculadora.java
+++ Calculadora.java
@@ -4,7 +4,7 @@ public class Calculadora implements CalculadoraInterface
 {
     
     public int suma(int n1, int n2){
-        return 0;
+        return n1 + n2;
     }
     public int resta(int n1, int n2){
         return 0;

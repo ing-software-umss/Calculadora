c6b24a26860cbb5c8b22418bf4cbea75e438563f
Carlos Cuba Cespedes <carloscubacespedes87@gmail.com>
2019-03-26 08:02:06 -0400

    agregando residuo

--- Calculadora.java
+++ Calculadora.java
@@ -7,16 +7,16 @@ public class Calculadora implements CalculadoraInterface
         return n1 + n2;
     }
     public int resta(int n1, int n2){
-        return n1-n2;
+        return n1 - n2;
     }
     public int mult(int n1, int n2){
-        return n1*n2;
+        return n1 * n2;
     }
     public int division(int n1, int n2){
-        return n1/n2;
+        return n1 / n2;
     }
     public int residuo(int n1, int n2){
-        return 0;
+        return n1 %n 2;
     }
     
 }
